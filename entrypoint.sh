85d5b78eb9427a0903b29cc9e770cc734b2775e3
Aaditya-Amzn <108915364+Aaditya-Amzn@users.noreply.github.com>
2022-12-16 11:44:33 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,39 +1,40 @@
 #!/bin/sh
 
 set -ue
+echo "Here"
 
-RepositoryName="${INPUT_REPOSITORY_NAME}"
-AwsRegion="${INPUT_AWS_REGION}"
-SourceBranch="${INPUT_SOURCE_BRANCH}"
-DestinationBranch="${INPUT_DESTINATION_BRANCH}"
-FoldersToCopy="${INPUT_FOLDERS_TO_COPY}"
-CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${RepositoryName}"
-echo "${github.event.before}"
-echo "${github.event.after}"
-CommitMessage="syncing commits for range ${github.event.before} to ${github.event.after}"
-git config --global --add safe.directory /github/workspace
-git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
-git config --global credential.UseHttpPath true
+# RepositoryName="${INPUT_REPOSITORY_NAME}"
+# AwsRegion="${INPUT_AWS_REGION}"
+# SourceBranch="${INPUT_SOURCE_BRANCH}"
+# DestinationBranch="${INPUT_DESTINATION_BRANCH}"
+# FoldersToCopy="${INPUT_FOLDERS_TO_COPY}"
+# CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${RepositoryName}"
+# echo "${github.event.before}"
+# echo "${github.event.after}"
+# CommitMessage="syncing commits for range ${github.event.before} to ${github.event.after}"
+# git config --global --add safe.directory /github/workspace
+# git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
+# git config --global credential.UseHttpPath true
 
-ActionEvent = "${github.event}"
+# ActionEvent = "${github.event}"
 
-git clone "$CodeCommitUrl"
-git checkout "$DestinationBranch"
+# git clone "$CodeCommitUrl"
+# git checkout "$DestinationBranch"
 
-cd "$RepositoryName"
+# cd "$RepositoryName"
 
-if [$FoldersToCopy]
-then
-    for folder in $FoldersToCopy
-    do
-        echo $folder
-        cp -r "/github/workspace/$folder" .
-    done
-else
-    cp -r "/github/workspace" .  
-fi
+# if [$FoldersToCopy]
+# then
+#     for folder in $FoldersToCopy
+#     do
+#         echo $folder
+#         cp -r "/github/workspace/$folder" .
+#     done
+# else
+#     cp -r "/github/workspace" .  
+# fi
 
-echo $CommitMessage
+# echo $CommitMessage
     
 # git remote add sync ${CodeCommitUrl}
 # git add .
