a1be17d3ef444572a058304e0eab6f3ed30d7300
Aaditya Shrivastava <aadified@amazon.com>
2022-12-16 16:12:39 +0530

    logged github

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,7 @@ CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${Repo
 github="${INPUT_GITHUB}" 
 echo $CodeCommitUrl
 echo "github ---- "
-event=${github["event"]}
-echo $event
+echo $github
 echo "----"
 CommitMessage="syncing commits for range ${github.event.before} to ${github.event.after}"
 # git config --global --add safe.directory /github/workspace
