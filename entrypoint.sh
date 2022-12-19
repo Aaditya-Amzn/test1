c67220a15b5d4b493ad28cdd13cd79279969e57e
Aaditya Shrivastava <aadified@amazon.com>
2022-12-19 16:27:01 +0530

    updated action and sh file inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,18 +7,23 @@ AwsRegion="${INPUT_AWS_REGION}"
 SourceBranch="${INPUT_SOURCE_BRANCH}"
 DestinationBranch="${INPUT_DESTINATION_BRANCH}"
 FoldersToCopy="${INPUT_FOLDERS_TO_COPY}"
-CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${RepositoryName}"
 github_after="${INPUT_GITHUB_AFTER}" 
 github_before="${INPUT_GITHUB_BEFORE}" 
+automated_user_name="${INPUT_AUTOMATED_USER_NAME}" 
+automated_user_email="${INPUT_AUTOMATED_USER_EMAIL}" 
+
+CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${RepositoryName}"
 CommitMessage="syncing commits for range ${github_before} to ${github_after}"
+
 git config --global --add safe.directory /github/workspace
 git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 
+#Configuring source repository with the input branch
 cd /github/workspace/
-git status
-git branch
+git checkout "$SourceBranch"
 
+#Configuring destination repository with the destination branch
 cd /
 git clone "$CodeCommitUrl"
 cd "$RepositoryName"
@@ -36,7 +41,10 @@ else
     done
 fi
 
-git remote add sync ${CodeCommitUrl}
-git add .
-git commit -m "${CommitMessage}"
-git push sync ${DestinationBranch}
+git config user.email "$automated_user_email"
+git config user.name "$automated_user_name" 
+
+# git remote add sync ${CodeCommitUrl}
+# git add .
+# git commit -m "${CommitMessage}"
+# git push sync ${DestinationBranch}
